a739d3310bbf355adbe5fe8ef3e3a1252ab4aad2
ariyansuhan009 <ariyansuhan009@gmail.com>
2021-09-20 19:44:44 +0600

    update with login

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,29 +1,31 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import '../Product/Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
+import { Link } from 'react-router-dom';
 
 const Product = (props) => {
-    //console.log(props);
-    
-
-    const {img, name, seller, price, stock} = props.product;
+    // console.log(props);
+    const { img, name, seller, price, stock, key } = props.product;
     return (
         <div className="product">
             <div>
-                <img src={props.product.img} alt="" />
+                <img src={img} alt="" />
             </div>
-
-            <div className="product-details">
-                <h4 className="product-name">{props.product.name}</h4>
+            <div>
+                <h4 className="product-name"><Link to={"/product/"+key}>{name}</Link></h4>
                 <br />
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <p><small>Only {stock} left in stock - Order soon</small></p>
-                <button className="main-button" onClick = {() => props.handelAddProduct(props.product)}> 
-                    <FontAwesomeIcon icon={faShoppingCart}/>Add to Cart
-                </button>
+                { props.showAddToCart === true && <button 
+                    className="main-button" 
+                    onClick={() => props.handleAddProduct(props.product)}
+                    > 
+                        <FontAwesomeIcon icon={faShoppingCart} /> add to cart
+                    </button>}
             </div>
+
         </div>
     );
 };
